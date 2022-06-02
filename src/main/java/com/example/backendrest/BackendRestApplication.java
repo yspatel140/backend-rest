776470e73d4bec2772adcc0fe3d90cc4498ab7a6
yspatel140 <Yogesh@4944>
2022-06-02 10:03:25 +0100

    initial commit backend rest

---
+++ src/main/java/com/example/backendrest/BackendRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendRestApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/backendrest/controller/BackendController.java
@@ -0,0 +1,32 @@
+package com.example.backendrest.controller;
+
+import com.example.backendrest.model.Person;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class BackendController {
+    private Map<String, Person> personMap = new HashMap<>();
+    {
+        personMap.put("Yogesh", new Person("Yogesh", 31, "London"));
+        personMap.put("Raj", new Person("Raj", 35, "Mumbai"));
+        personMap.put("Tom", new Person("Tom", 34, "Paris"));
+    }
+
+    @RequestMapping(value = "/person/{name}", method = RequestMethod.GET)
+    public Person personById(@PathVariable String name) {
+        return personMap.get(name);
+    }
+
+    @RequestMapping(value = "/person", method = RequestMethod.GET)
+    public List<Person> personList() {
+        return new ArrayList<>(personMap.values());
+    }
+}
